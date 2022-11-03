91917e2b1befcefbee9f56ef0fd710f17277aa01
raphaelglt <raphael.guillot@edu.esiee-it.fr>
2022-11-03 15:29:10 +0100

    connexion to connection

--- sql/register.sql
+++ sql/register.sql
@@ -1,2 +1,2 @@
-INSERT INTO Utilisateurs (user_email, user_password, user_pseudo, user_register_date, user_last_connexion)
+INSERT INTO Utilisateurs (user_email, user_password, user_pseudo, user_register_date, user_last_connection)
 VALUES ("test@gmail.com", "test", "testpasswordverysecret", "2022-11-03 12:36:24", "2022-11-03 12:36:24");
