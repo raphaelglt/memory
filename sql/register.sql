50abe68b6f9e5fc34e2afeb46ebea557f00dbb38
raphaelglt <raphael.guillot@edu.esiee-it.fr>
2022-11-03 15:27:01 +0100

    sql folder init

---
+++ sql/register.sql
@@ -0,0 +1,2 @@
+INSERT INTO Utilisateurs (user_email, user_password, user_pseudo, user_register_date, user_last_connexion)
+VALUES ("test@gmail.com", "test", "testpasswordverysecret", "2022-11-03 12:36:24", "2022-11-03 12:36:24");
