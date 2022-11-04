e77ff60aca1bb30866b6b0b76c54c059952ff98b
Fabien <fab.arnal@free.fr>
2022-11-04 10:26:07 +0100

    update score.sql

--- sql/score.sql
+++ sql/score.sql
@@ -1,3 +1,3 @@
 INSERT INTO Scores (score_user_id, score_game_id, score_level, score_value, score_datetime)
-VALUES (« 1 », « 1 », « 2 », "2022-11-03 12:36:24");
+VALUES ("1", "1", "2", "2022-11-03 12:36:24");
 --change values by input values
