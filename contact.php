56ecb51ba01e7446aa71c699c243fe01741ac4cd
Fabien <fab.arnal@free.fr>
2022-11-10 15:40:50 +0100

    update verif nom email form contact.php

--- contact.php
+++ contact.php
@@ -41,13 +41,40 @@ session_destroy();
         </div>
 
         <div class="form-container">
-            <form action="">
-                <input type="text" name="nom" placeholder="Nom" id="form"></br>
+            <form method ="post">
+                <input type="text" name="nom" placeholder="Nom" id="form">
                 <input type="text" name="email" placeholder="Email" id="form"></br>
                 <input type="text" name="sujet" placeholder="Sujet" id="form"></br>
                 <textarea name="comm" placeholder="Message" id="form"></textarea></br>
                 <button id="buttonsend"><a href="">Envoyer</a></button>
             </form>
+            <p id="message">
+                <?php
+                if(empty($_POST)){
+
+                    if (empty($_POST["name"])) {
+
+                        echo '<p style = color : white;> Veuillez vérifier le formulaire - veuillez saisir votre nom.';
+
+                    }else if (empty($_POST["email"])){
+                        if (!empty($_POST["email"])) {
+                            $point = strpos($_POST['nom'], ".");
+                            $arobase = strpos($_POST[''], "@");
+                            if ($point === false)
+                                echo '<p style= color: white;">Veuillez vérifier le formulaire - votre email doit comporter un point.</p>';
+                            else if ($arobase === false)
+                                echo '<p style= color: white;">Veuillez vérifier le formulaire - votre email doit comporter un arobase.</p>';
+                            else
+                                echo '<p style= color: white;">Veuillez vérifier le formulaire - votre email est : </p>' . $_POST['email'];
+                        } else {
+                            echo '<p style= color: white;">Veuillez vérifier le formulaire - veuillez saisir un email.</p>';
+                        }
+                    }
+
+                }
+
+                ?>
+            </p>
         </div>
     </section>
     <?php
