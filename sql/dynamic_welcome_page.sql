0da3d6072fa4e064731929054a407ff5619de778
raphaelglt <raphael.guillot@edu.esiee-it.fr>
2022-11-15 09:08:39 +0100

    users connected and record time

--- sql/dynamic_welcome_page.sql
+++ sql/dynamic_welcome_page.sql
@@ -1,3 +1,5 @@
 SELECT 
 (SELECT COUNT(*) FROM Scores) as count_scores,
-(SELECT COUNT(*) FROM Utilisateurs) as count_users;
+(SELECT COUNT(*) FROM Utilisateurs) as count_users,
+(SELECT score_stopwatch FROM Scores WHERE score_level = 'impossible' GROUP BY score_stopwatch ASC LIMIT 1) as best_score,
+(SELECT COUNT(*) FROM Utilisateurs WHERE user_last_connection > date_sub(now(), interval 5 minute)) as users_connected
