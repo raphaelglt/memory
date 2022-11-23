5bfbc0f4fa417e2072864085491bc3a804fa986b
raphaelglt <raphael.guillot@edu.esiee-it.fr>
2022-11-23 11:31:41 +0100

    maj

--- memory.php
+++ memory.php
@@ -5,63 +5,8 @@ include('./init.php');
 $stmt = $dbh->query('SELECT DISTINCT image_theme FROM Images');
 $themes = $stmt->fetchAll();
 
-if (isset($_SESSION['user_id'])) {
-    $sql = file_get_contents('./sql/get_images.sql');
-    $stmt = $dbh->prepare($sql);
-    $theme = "capybara";
-    $stmt->bindParam(':image_theme', $theme);
-    $stmt->execute();
-
-    $memory_size = 10;
-    $images_needed = intdiv(pow($memory_size, 2),2);
-
-    $row_count = $stmt->rowCount();
-    if ($row_count<$images_needed) {
-        $difference = $images_needed-$row_count;
-        if ($memory_size>=10) {
-            $photo_nb = 50;
-            $page_nb = intdiv($difference, $photo_nb);
-            if (($difference)%$photo_nb!=0) {
-                $page_nb++;
-            }
-        } else {
-            $photo_nb = $difference;
-            $page_nb = 1;
-        }
-
-        $sql = file_get_contents('./sql/insert_image.sql');
-        $stmt = $dbh->prepare($sql);
-
-        $next_link = "https://api.pexels.com/v1/search?query=$theme&page=$page_nb&per_page=$photo_nb";
-        for ($page=0; $page<$page_nb; $page++) {
-            $curl = curl_init($next_link);
-
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, [
-            'X-RapidAPI-Host: kvstore.p.rapidapi.com',
-            'Authorization: 563492ad6f9170000100000193f0dcb4e9494a82b0b266c32b3448e7',
-            'Content-Type: application/json'
-            ]);
-
-            $response = curl_exec($curl);
-            $decode = json_decode($response, true);
-            curl_close($curl);
-            if (!isset($decode['error'])) {
-                foreach ($decode['photos'] as $elt) {
-                    $stmt->bindParam(':image_id', $elt['id']);
-                    $stmt->bindParam(':image_theme', $theme);
-                    $stmt->bindParam(':image_url', $elt['src']['original']);
-                    $stmt->execute();
-                }
-                if (!empty($decode['next_page'])) $next_link = $decode['next_page']; 
-            } else {
-                echo "Error";
-            }
-        }
-    }
-    $sql = file_get_contents('./sql/select_chat.sql');
-    $stmt = $dbh->query($sql);
-}    
+$sql = file_get_contents('./sql/select_chat.sql');
+$stmt = $dbh->query($sql); 
 ?>
 <!DOCTYPE html>
 <html>
